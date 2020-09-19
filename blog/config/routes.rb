adbbe251d2fa27a6b9476c0af407ec0863cd27cb
Renante D. Entera <renantee@codev.com>
2020-09-19 11:28:26 +0800

    Course Project: Blog application (Updated - part 10)
    Update includes:
    > Front-end Page Customization
    > Create "Read Post" Page

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 	devise_for :authors
 	get 'home/index'
 	root to: "readers/home#index"
+	get '/blog/:id' => 'readers/posts#show', as: :blog_post
 
 	scope module: 'authors' do
 		resources :posts do
