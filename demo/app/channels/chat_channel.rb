266f9bc1827e9e2400d2c1a46c349a8f058314b7
Renante D. Entera <renantee@codev.com>
2020-09-26 13:25:50 +0800

    Rails Guides: Testing and debugging (part 5)
    This is a quick demo for Rails Guides (Other Components):
    > Action Cable Overview

---
+++ demo/app/channels/chat_channel.rb
@@ -0,0 +1,11 @@
+class ChatChannel < ApplicationCable::Channel
+  # Called when the consumer has successfully
+  # become a subscriber to this channel.
+  def subscribed
+    stream_from "chat_#{params[:room]}"
+  end
+
+  def receive(data)
+    ActionCable.server.broadcast("chat_#{params[:room]}", data)
+  end
+end
