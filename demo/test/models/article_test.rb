bbe506e3f6e135215ffd67544f5f2ae337dccdf3
Renante D. Entera <renantee@codev.com>
2020-09-29 11:58:00 +0800

    Rails Guides: Testing and debugging (part 7)
    This is a quick demo for Rails Guides (Digging Deeper):
    > Testing Rails Applications
    
    Executed commands:
    > rails generate model article title:string body:text
    > rails db:migrate RAILS_ENV=test
    > rails test test/models/article_test.rb

---
+++ demo/test/models/article_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class ArticleTest < ActiveSupport::TestCase
+  test "the truth" do
+    assert true
+  end
+
+  test "should not save article without title" do
+    article = Article.new
+    assert_not article.save, "Saved the article without a title"
+  end
+
+  test "should report error" do
+    # some_undefined_variable is not defined elsewhere in the test case
+    assert_raises(NameError) do
+      some_undefined_variable
+    end
+  end
+end
